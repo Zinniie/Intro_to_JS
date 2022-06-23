b471c4004404dd5b4200f494ea882394ece799ba
Zinniie <ezinne.blessing@yahoo.com>
2022-06-23 21:54:22 +0100

    IntroJS

---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = "Ezinne Blessing Nnabugwu";
+const myHeight = "5ft 6inches";
+const myCountry = "Nigeria";
+
+alert("My Name is " + myName+"\nMy Height is " + myHeight+"\nMy Country is " + myCountry)
